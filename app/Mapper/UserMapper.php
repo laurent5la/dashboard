7745267517abe5d7934552407cfc3ea4ed61a2dd
Michael Valenzuela <mvalenzuela@mvalenzuela-MBPR.local>
2016-09-08 10:35:48 -0700

    WAC-657: changed referenced to UserFactory to UserObjectFactory

--- app/Mapper/UserMapper.php
+++ app/Mapper/UserMapper.php
@@ -5,42 +5,42 @@ class UserMapper
 {
     public function getUser($params)
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->retrieveUserInfo($params);
         return $userObject;
     }
 
     public function setUser($params)
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->storeUserInfo($params);
         return $userObject;
     }
 
     public function updateUserPersonalInformation($params)
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->updateUserPersonalInfo($params);
         return $userObject;
     }
 
     public function logoutUser()
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->logoutUser();
         return $userObject;
     }
 
     public function forgotPassword($params)
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->createByEmail($params);
         return $userObject;
     }
 
     public function changePassword($params)
     {
-        $userFactory = new UserFactory();
+        $userFactory = new UserObjectFactory();
         $userObject = $userFactory->changePassword($params);
         return $userObject;
     }
