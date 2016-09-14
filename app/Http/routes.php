17c8649039b01c3b72bf0903740d6ca244314472
Michael Valenzuela <mvalenzuela@mvalenzuela-MBPR.local>
2016-09-14 13:40:53 -0700

    Grouped routes into version prefix and removed gulpfile.js and package.json

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::post('/user/reset-password', 'UserController@resetPassword');
-Route::post('/user/change-password', 'UserController@changePassword');
-Route::post('/user/login', 'UserController@login');
-Route::post('/user/logout', 'UserController@logout');
-Route::post('/user/register', 'UserController@register');
+Route::group(['prefix' => '/v1'],  function() {
+    Route::post('/user/reset-password', 'UserController@resetPassword');
+    Route::post('/user/change-password', 'UserController@changePassword');
+    Route::post('/user/login', 'UserController@login');
+    Route::post('/user/logout', 'UserController@logout');
+    Route::post('/user/register', 'UserController@register');
+});
